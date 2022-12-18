1f480fa381953c663b6435ba0b879851a2b14aad
Malcolm Keyes <3.malcolmk@gmail.com>
2022-12-17 20:41:40 -0500

    change TankState to include battlefield and have more state methods

--- tank.py
+++ tank.py
@@ -1,3 +1,5 @@
+from battlefield import Battlefield
+
 class Tank:
 
     def __init__(self) -> None:
@@ -13,24 +15,32 @@ class Tank:
 
 class TankState:
 
-    def __init__(self, tank: Tank, position: "tuple[int, int]") -> None:
+    def __init__(self, tank: Tank, position: "tuple[int, int]", battlefield: Battlefield) -> None:
         self.tank: Tank = tank
+        self.battlefield: Battlefield = battlefield
 
-        self.moving: bool = False
-        self.in_light_cover: bool = False
-        self.in_heavy_cover: bool = False
-
-        self.time_to_reloaded: int = tank.reload_time
+        self.time_until_reloaded: int = tank.reload_time
         self.position: tuple[int, int] = position
+        self.last_position: tuple[int, int] = position
 
         self.remaining_health: int = tank.health
         self.damage_dealt_and_avoided: int = 0
         self.damage_taken: int = 0
 
+    def in_heavy_cover(self) -> bool:
+        return self.battlefield.position_is_heavy_cover(self.position)
+
+    def in_light_cover(self) -> bool:
+        return self.battlefield.position_is_light_cover(self.position)
+
+    def moving(self) -> bool:
+        return self.position != self.last_position
+
     def ready_to_shoot(self) -> bool:
-        return (not self.in_heavy_cover) and self.time_to_reloaded == 0
+        return (not self.in_heavy_cover()) and self.time_until_reloaded == 0
 
     def alive(self) -> bool:
         return self.remaining_health > 0
 
-    
+    # def take_action(self, new_position: tuple[int,int] | None = None, ):
+    #     pass
