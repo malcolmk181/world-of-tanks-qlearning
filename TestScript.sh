2b731eedc9b4801f89a9cb487280faa97e1f2efc
Malcolm Keyes <3.malcolmk@gmail.com>
2022-12-21 15:18:52 -0500

    add make check to testscript

--- TestScript.sh
+++ TestScript.sh
@@ -1,19 +1,25 @@
-echo "Hi Professor Glenn! Happy Holidays!"
-echo "This test script will take about a minute to run, and will show you a Greedy vs Random agent,"
-echo "then a pretrained q-learning agent against the random agent, then the q-learning agent against the greedy agent."
-echo "More information is availble in the README."
-echo
+FILE=WorldOfTanks
 
-echo "Results of 10k battles of Greedy agent vs Random agent:"
-./WorldOfTanks --get-baselines --greedy-v-random
+if [ -f "$FILE" ]; then
+    echo "Hi Professor Glenn! Happy Holidays!"
+    echo "This test script will take about a minute to run, and will show you a Greedy vs Random agent,"
+    echo "then a pretrained q-learning agent against the random agent, then the q-learning agent against the greedy agent."
+    echo "More information is availble in the README."
+    echo
 
-echo
-echo "Results of 10k battles of pre-trained (on 100k battles) Q-learning agent vs random agent:"
-./WorldOfTanks --train-q-learning --q-v-random --use-pretrained
+    echo "Results of 10k battles of Greedy agent vs Random agent:"
+    ./WorldOfTanks --get-baselines --greedy-v-random
 
-echo
-echo "Results of 10k battles of pre-trained (on 100k battles) Q-learning agent vs greedy agent:"
-./WorldOfTanks --train-q-learning --q-v-greedy --use-pretrained
+    echo
+    echo "Results of 10k battles of pre-trained (on 100k battles) Q-learning agent vs random agent:"
+    ./WorldOfTanks --train-q-learning --q-v-random --use-pretrained
 
-echo
-echo "More options, including training your own model, available in the README."
+    echo
+    echo "Results of 10k battles of pre-trained (on 100k battles) Q-learning agent vs greedy agent:"
+    ./WorldOfTanks --train-q-learning --q-v-greedy --use-pretrained
+
+    echo
+    echo "More options, including training your own model, available in the README."
+else 
+    echo "Please run make before running this script."
+fi
